a70294d7bce879f7a155abcfc474bf83d2085b27
Marcin Moskała <marcinmoskala@gmail.com>
2018-02-15 12:19:59 +0100

    Use filterIndexed and improve tests

--- src/org/kotlin99/lists/P16.kt
+++ src/org/kotlin99/lists/P16.kt
@@ -5,12 +5,11 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
 fun <T> drop(n: Int, list: List<T>): List<T> = list
-    .mapIndexed { i: Int, value: T -> Pair(i, value) }
-    .filter { (it.first + 1) % n != 0 }
-    .map { it.second }
+        .filterIndexed { i: Int, value: T -> (i + 1) % n != 0 }
 
 class P16Test {
     @Test fun `drop every Nth element from a list`() {
         assertThat(drop(3, "abcdefghijk".toList()), equalTo("abdeghjk".toList()))
+        assertThat(drop(3, (1..4).toList())), equalTo(listOf(1, 2, 4)))
     }
 }
